d44de1afcb538386b0756c2944d2ceee599a6ee9
i.q <64720410+klanrali@users.noreply.github.com>
2021-05-30 16:50:03 +0300

    Update _setbot.py

--- resources/setbot/_setbot.py
+++ resources/setbot/_setbot.py
@@ -1,17 +1,3 @@
-#    Copyright (C) KIMO ~ ANL0KE 2021
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as published by
-#    the Free Software Foundation, either version 3 of the License, or
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
-
-
 import asyncio
 import telethon.utils
 from userbot import LOGS, bot
@@ -60,6 +46,3 @@ async def setbot():
         except Exception as e:
             LOGS.info(str(e))
 
-# End For icss bot Only
-# Owner ~ ANL0KE
-# Tele ~ @NIIIN2
